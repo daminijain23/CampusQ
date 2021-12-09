1036ccb1adf01c20f06079668b61e57c8f389938
daminijain23 <dajain@redhat.com>
2021-12-08 23:16:47 -0500

    Update Restaurant.java

--- Restaurant.java
+++ Restaurant.java
@@ -3,12 +3,22 @@ package guide;
 import java.util.LinkedList;
 import java.util.Queue;
 
+
+/** 
+ * Represents a Restaurant
+ * @author Ashesh Sheth, Damini Jain, Divya Muralidharan
+*/
 public class Restaurant {
 
     public Queue<Integer> queueSizes;
     public int outputQueueSize;
     public int sumOfCurrentBlock;
 
+    /** 
+     * @params queueSizes A queue where a user's input get populated
+     * @params sumOfCurrentBlock Sum of current five values in the queue
+     * @params outputQueueSize Average of current five values in the queue. Current queue length of a restuarnt
+    */
     public Restaurant()
     {
         outputQueueSize = 0;
